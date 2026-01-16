aa40b77ccdcd00a10b8cea1561601ecdd28c539b
swild <sebastian.wild@axians.at>
2026-01-16 20:04:39 +0100

    feat(3-04): create slices barrel export
    - Export all 4 slice creators from single location
    - createConfigSlice, createEndpointSlice, createFslogixSlice, createUiSlice

---
+++ src/store/slices/index.ts
@@ -0,0 +1,4 @@
+export { createConfigSlice } from './configSlice';
+export { createEndpointSlice } from './endpointSlice';
+export { createFslogixSlice } from './fslogixSlice';
+export { createUiSlice } from './uiSlice';
